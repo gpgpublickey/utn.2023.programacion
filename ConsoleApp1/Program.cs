a5e057c3093f56ad9bc468c8d7657e3f6c01d8e8
dariogabrielnunez <132526136+dariogabrielnunez@users.noreply.github.com>
2023-05-03 21:47:43 -0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+unsafe {
+    int x = 11;
+    int* a = &x;
+    Console.WriteLine("{0}", *a);
+}
+
+Console.ReadLine();
