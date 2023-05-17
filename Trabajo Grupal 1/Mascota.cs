bb3740343950a9b9c834d0580c6b37539c1125a7
Mati279 <104004760+Mati279@users.noreply.github.com>
2023-05-17 20:08:37 -0300

    Initial Commit - Tarea Grupal 1

---
+++ Trabajo Grupal 1/Mascota.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Trabajo_Grupal_1
+{
+    internal class Mascota
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public Especie Especie { get; set; }
+
+        public string ObtenerNombre()
+        {
+            return this.Nombre;
+        }
+
+        public int ObtenerEdad()
+        {
+            return this.Edad;
+        }
+
+        public Especie ObtenerEspecie()
+        {
+            return this.Especie;
+        }
+
+
+        public void DarNombre(string nombre)
+        {
+
+            this.Nombre = nombre;
+        }
+
+        public void DarEdad(int edad)
+        {
+            this.Edad = edad;
+        }
+
+        public void DarEspecie(Especie especie)
+        {
+            this.Especie = especie;
+        }
+
+        public void HacerSonido()
+        {
+            if (this.Especie.Nombre == "Gato")
+            {
+                Console.WriteLine("Miauuu");
+            }
+            else
+                Console.WriteLine("Guauuu");
+        }
+
+        public void Moverse()
+        {
+            Console.WriteLine(this.Nombre + " se mueve.");
+        }
+
+
+
+    }
+}
---
+++ Trabajo Grupal 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using Trabajo_Grupal_1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Perros.
+        Especie perro = new Especie("Perro");
+        //Gatos.
+        Especie gato = new Especie("Gato");
+
+        //Perro Loli.
+        Mascota Mascota1 = new Mascota();
+        Mascota1.DarNombre("Loli");
+        Mascota1.DarEdad(5);
+        Mascota1.DarEspecie(perro);
+
+        //Gata Sanah.
+        Mascota Mascota2 = new Mascota();
+        Mascota2.DarNombre("Sanah");
+        Mascota2.DarEdad(3);
+        Mascota2.DarEspecie(gato);
+
+        //Llamar métodos.
+        Mascota1.HacerSonido();
+        Mascota1.Moverse();
+
+        Mascota2.HacerSonido();
+        Mascota2.Moverse();
+
+
+        Console.ReadLine();
+
+    }
+}
