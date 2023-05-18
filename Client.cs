a09ed8d84be2069f6b72e517407e251ae15b609a
Rhonal Gonzalez <rhonalgnzl@gmail.com>
2023-05-17 23:31:25 -0300

    Bank and Client added, bank working on Program.cs

---
+++ Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+
+	public class Client
+    {
+        public int Id { get; set; }
+
+        private double balance;
+
+		public double Balance
+		{
+			get { return balance; }
+			set { balance = value; }
+		}
+
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		public Client(int id, string name, double balance) {
+			this.Id = id;
+			this.balance = balance;
+			this.name = name;
+		}
+
+	}
+}
