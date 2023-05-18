e83d80cee290bc49677630e816f1121ea6413f0d
dariogabrielnunez <132526136+dariogabrielnunez@users.noreply.github.com>
2023-05-18 20:59:55 -0300

    Add files via upload

---
+++ EjercicioClaseEquipo1-Coche/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase3
+{
+    public class Coche
+    {
+        public string Modelo { get; set; }
+        public string Marca { get; set; }
+        public int Año { get; set; }
+        public double Velocidad { get; set; }
+        public string Marcha { get; set; }
+
+        public void Acelerar(double aceleracion)
+        {
+            if (this.Marcha == "N") 
+            {
+                Console.WriteLine("No se puede acelerar en punto Muerto (N).");
+            }
+            else
+            {
+                Velocidad += aceleracion;
+                Console.WriteLine("(Acelerado) - Velocidad Actual: " + Velocidad + "km/h");
+            }
+        }
+
+        public void Frenar(double desaceleracion) {
+            if (desaceleracion <= Velocidad)
+            {
+                Velocidad -= desaceleracion;
+                Console.WriteLine("(Frenado) - Velocidad Actual: " + Velocidad + "km/h");
+            }
+            else {
+                Velocidad = 0;
+                Console.WriteLine("El coche esta totalmente detenido.");
+            }
+        }
+
+        public void pasarMarcha(String marcha) {
+            switch (marcha) { 
+                case "N":
+                    Marcha = marcha;
+                    Console.WriteLine("Transmision en punto muerto.");
+                    break;
+                case "1":
+                    Marcha = marcha;
+                    Console.WriteLine("Primera marcha.");
+                    break;
+                case "2":
+                    Marcha = marcha;
+                    Console.WriteLine("Segunda marcha.");
+                    break;
+                case "3":
+                    Marcha = marcha;
+                    Console.WriteLine("Tercera Marcha");
+                    break;
+                case "4":
+                    Marcha = marcha;
+                    Console.WriteLine("Cuarta marcha.");
+                    break;
+                case "5":
+                    Marcha = marcha;
+                    Console.WriteLine("Quinta marcha.");
+                    break;
+                case "R":
+                    Marcha = marcha;
+                    Console.WriteLine("Reversa acoplada.");
+                    break;
+                default:
+                    Console.WriteLine("Marcha Ingresada no valida. (N/R/[1-5])");
+                    break;
+            }
+        }
+    }
+}
---
+++ EjercicioClaseEquipo1-Coche/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace EjercicioClase3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Coche coche = new Coche();
+
+            coche.Modelo = "R12";
+            coche.Marca = "Renault";
+            coche.Año = 1980;
+
+            coche.pasarMarcha("N");
+            coche.Acelerar(130);
+            coche.Frenar(30);
+
+            Console.WriteLine("\nMarca: " + coche.Marca);
+            Console.WriteLine("Modelo: " + coche.Modelo);
+            Console.WriteLine("Año: " + coche.Año.ToString());
+        }
+    }
+}
