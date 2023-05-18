c2586643e7d0a0be1a2365de13db590e24a6b168
VSerra12 <vicky.serra.24@gmail.com>
2023-05-17 21:02:51 -0300

    Ejercicio 1

---
+++ ConsoleApp1/Music.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{ 
+	public class Music
+	{
+		private String tittle { get; set; }
+		private Artist artist { get; set; }
+		private int duration { get; set; }
+		private String gender { get; set; }
+
+		public Music(Artist artist, String tittle, String gender, int duration){
+			this.artist = artist;
+			this.tittle = tittle;
+			this.gender = gender;
+			this.duration = duration;
+		}
+
+		public void Play() { }
+
+		public void Stop() { }
+
+		public void Pause() { }
+
+	}
+
+}
