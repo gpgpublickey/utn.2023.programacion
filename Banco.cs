faa388a6db50929045e8257a4f587d5b93c75103
Rhonal Gonzalez <rhonalgnzl@gmail.com>
2023-05-18 12:07:44 -0300

    new test for Bank in Program.cs

--- Banco.cs
+++ Banco.cs
@@ -1,53 +1,121 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
 
 public class Banco
 {
-    string name;
-    string direction;
-    //List<Client> clients; 
-    int clientsQuantity;
+    private string name;
+    private string direction;
+    public List<Client> clients { get; set; }
+    private int clientsQuantity;
     public Banco()
 	{
+            this.clients = new List<Client>();
+            this.clientsQuantity = 0;
+    }
+
+    public Banco(string name, string direction, int clients)
+	{
+            this.name = name;
+            this.direction = direction;
+            this.clientsQuantity = 0;
+            this.clientsQuantity = clients;
+            this.clients = new List<Client>();
+            for(int i = 0; i < clients; i++)
+            {
+                this.clients.Add(new Client(i+1, "Client"+(i+1), 0));
+
+            }
 	}
 
     //setters
-    public void setName(string name)
+    public void SetName(string name)
     {
         this.name = name;
     }
-    public void setDirection(string direction)
+    public void SetDirection(string direction)
     {
         this.direction = direction;
     }
-    public void setClientsQuantity(int quantity)
+    public void SetClientsQuantity(int quantity)
     {
         this.clientsQuantity = quantity;
     }
     //getters
 
-    public string getName()
+    public string GetName()
     {
         return this.name;
     }
-    public string getDirection()
+    public string GetDirection()
     {
         return this.direction;
     }
-    public int getClientsQuantity()
+    public int GetClientsQuantity()
     {
         return this.clientsQuantity;
     }
 
-    public void addClient(/*Client client*/)
+    public void AddClient(Client client)
     {
-        //this.clients.Add(client);
+        this.clients.Add(client);
         this.clientsQuantity++;
+            Console.WriteLine("New client added! Welcome " + client.Name + "!");
+    }   
+
+    public void RemoveClient(Client client)
+    {
+        if(this.clientsQuantity <= 0)
+            {
+                Console.WriteLine("There is not clients");
+            }
+            else
+            {
+                if(clients.Contains(client)) { 
+                    this.clients.Remove(client);
+                    this.clientsQuantity--;
+                }
+                else
+                {
+                    Console.WriteLine("That client does not exist");
+                }
+            }
     }
 
-    public void removeClient(/*Client client*/)
+    public void DepositTo(Client client, double amount)
+        {
+            client.Balance += amount;
+        }
+
+    public void WithdrawFrom(Client client, double amount)
+        {
+            if(client.Balance >= amount)
+            {
+                client.Balance -= amount;
+
+            }
+            else
+            {
+                Console.WriteLine("Insufficient balance");
+            }
+        }
+    public void Transfer(Client client1, Client client2, double amount)
     {
-        //this.clients.Remove(client);
-        this.clientsQuantity--;
+            if(client1.Balance >= amount)
+            {
+                client1.Balance -= amount;
+                client2.Balance += amount;
+                Console.WriteLine("Successful transfer");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient balance");
+            }
     }
 }
 
@@ -56,3 +124,5 @@ public class Banco
  agregar y eliminar Clientes
 Realizar transsaciones como depositos, retiros y transferencias entre cuentas
  */
+
+}
