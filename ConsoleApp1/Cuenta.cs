6ae804e8bdabf8faed94379dec76e34229fa2715
fd <fchiesah@gmail.com>
2023-05-18 17:50:32 -0300

    adding cuenta class

---
+++ ConsoleApp1/Cuenta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Cuenta
+    {
+        public string NoCuenta { get; set; }
+        public string Propietario { get; set; }
+        public decimal Saldo { get; set; }
+
+        public decimal Depositar(decimal deposito)
+        {
+            Saldo += deposito;
+            return Saldo;
+        }
+
+        public decimal Retirar(decimal retiro)
+        {
+            if (Saldo > retiro)
+            {
+                Saldo -= retiro;
+                return Saldo;
+            }
+            else
+            {
+                Console.WriteLine("Fondos Insuficientes");
+            }
+            return Saldo;
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,31 @@
 ﻿namespace ConsoleApp1
-{
+{ 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // CLASE Cuenta
+            Cuenta cuenta = new Cuenta();
+
+            // Setters
+
+            cuenta.NoCuenta = "9876543210";
+            cuenta.Propietario = "Gerson";
+            cuenta.Saldo = 1000;
+
+            // Getters
+
+            Console.WriteLine("Numero de Cuenta: " + cuenta.NoCuenta);
+            Console.WriteLine("Usuario: " + cuenta.Propietario);
+            Console.WriteLine("Dinero en cuenta: " + cuenta.Saldo);
+
+            cuenta.Depositar(1200);
+            Console.WriteLine("El saldo ahora es de: " + cuenta.Saldo);
+
+            cuenta.Retirar(2000);
+            Console.WriteLine("El saldo ahora es de: " + cuenta.Saldo);
+
         }
     }
 }
