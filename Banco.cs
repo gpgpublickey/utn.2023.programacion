3e70e56076bc1e0d5142b2f50792d4611d41a51c
Rhonal Gonzalez <rhonalgnzl@gmail.com>
2023-05-17 21:29:42 -0300

    task 1705 Avion, Juego, Banco

---
+++ Banco.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+public class Banco
+{
+    string name;
+    string direction;
+    //List<Client> clients; 
+    int clientsQuantity;
+    public Banco()
+	{
+	}
+
+    //setters
+    public void setName(string name)
+    {
+        this.name = name;
+    }
+    public void setDirection(string direction)
+    {
+        this.direction = direction;
+    }
+    public void setClientsQuantity(int quantity)
+    {
+        this.clientsQuantity = quantity;
+    }
+    //getters
+
+    public string getName()
+    {
+        return this.name;
+    }
+    public string getDirection()
+    {
+        return this.direction;
+    }
+    public int getClientsQuantity()
+    {
+        return this.clientsQuantity;
+    }
+
+    public void addClient(/*Client client*/)
+    {
+        //this.clients.Add(client);
+        this.clientsQuantity++;
+    }
+
+    public void removeClient(/*Client client*/)
+    {
+        //this.clients.Remove(client);
+        this.clientsQuantity--;
+    }
+}
+
+/*nombre, direccion, nroCLientes, 
+ metodos: 
+ agregar y eliminar Clientes
+Realizar transsaciones como depositos, retiros y transferencias entre cuentas
+ */
