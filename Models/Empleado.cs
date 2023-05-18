57977ae41bf88edb88b2cae4e775324670fcb355
Melina Evangelista <evangelistamelinapaula@gmail.com>
2023-05-17 21:02:34 -0300

    Agrego clase Empleado

---
+++ Models/Empleado.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Models
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public float Salario { get; set; }
+
+
+        public Empleado(string nombre, float salario ) 
+        { 
+            Nombre = nombre;
+            Salario = salario;
+        }
+
+        public void AumentarSalario(Empleado empleado, float salarioNuevo) 
+        { 
+            if(empleado!=null)
+            {
+                empleado.Salario = salarioNuevo;
+            }
+            
+        }
+        public float CalcularSalarioAnual(Empleado empleado)
+        {
+            float salarioAnual = empleado.Salario * 12;
+            return salarioAnual;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using ConsoleApp.Models;
+
+class Program
+{
+
+    private static void Main(String[] args)
+    {
+        string nombre = "Pepe";
+        float salario = 25;
+       
+        Empleado empleado = new Empleado(nombre, salario);
+
+    }
+}
