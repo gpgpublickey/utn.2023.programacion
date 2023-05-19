8f7fa8315ba5ebcc5702079850b8b161a498de7c
Brenda Miranda <45616587+bmirandach@users.noreply.github.com>
2023-05-18 22:57:09 -0300

    Ejercicio clase Libro

---
+++ Libro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utn._2023.programacion
+{
+  public class Libro
+  {
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+    public string Editorial { get; set; }
+    public string ISBN { get; set; }
+
+    public void ImprimirInformacion() {
+      Console.WriteLine("Título: " + this.Titulo);
+      Console.WriteLine("Autor: " + this.Autor);
+      Console.WriteLine("Editorial: " + this.Editorial);
+      Console.WriteLine("ISBN: " + this.ISBN);
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace HelloWorld
+﻿namespace utn._2023.programacion
 {
   class Program
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello, World!");
+      Libro libro = new Libro();
+      libro.Autor = "Cosme Fulanito";
+      libro.Titulo = "Yo soy Cosme Fulanito";
+      libro.Editorial = "Planeta";
+      libro.ISBN = "1234567890";
+      libro.ImprimirInformacion();
     }
   }
 }
