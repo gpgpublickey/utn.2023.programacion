9b6c1e1ea3784eecce126fe0ceaab86b8f477611
dariogabrielnunez <132526136+dariogabrielnunez@users.noreply.github.com>
2023-05-19 22:58:13 -0300

    Add files via upload

---
+++ SolucionBanco/Banco.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoBanco
+{
+    public class Banco
+    {
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public int CantidadClientes { get; set; }
+        public List<Cliente> Clientes { get; set; }
+
+        public Banco(string nombre, string direccion) 
+        {
+            Nombre = nombre;
+            Direccion = direccion;
+            CantidadClientes = 0;
+            Clientes = new List<Cliente>();
+        }
+
+        public void AgregarCliente(Cliente cliente) 
+        { 
+            Clientes.Add(cliente);
+            CantidadClientes++;
+            Console.WriteLine("Se dio de alta con exito al cliente: " + cliente.Nombre);
+        }
+
+        public void EliminarCliente(Cliente cliente)
+        { 
+            Clientes.Remove(cliente);
+            CantidadClientes--;
+            Console.WriteLine("Se elimino con exito al cliente: " + cliente.Nombre);
+        }
+    }
+}
---
+++ SolucionBanco/CuentaBancaria.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoBanco
+{
+    public class CuentaBancaria
+    {
+        public string NumeroCuenta { get; set; }
+        public decimal Saldo { get; set; }
+
+        public CuentaBancaria(string numero) 
+        {
+            NumeroCuenta = numero;
+            Saldo = 0; 
+        }
+
+        public void Depositar(decimal monto)
+        { 
+            Saldo += monto;
+            Console.WriteLine("Deposito Realizado con exito. Su saldo actual es: $" + Saldo );
+        }
+
+        public void Retirar(decimal monto)
+        {
+            if (monto > Saldo)
+            {
+                Console.WriteLine("No hay fondos suficientes para realizar la extracción.");
+            }
+            else
+            {
+                Saldo -= monto;
+            }
+        }
+
+        public void Transferir(CuentaBancaria destino, decimal monto)
+        {
+            if (monto > Saldo)
+            {
+                Console.WriteLine("No puso realizarse la transferencia. Fondos Insuficientes.");
+            }
+            else
+            {
+                Saldo -= monto;
+                destino.Saldo += monto;
+                Console.WriteLine("Se realizo exitosamente una transferencia por $" + monto + " a la cuenta: " + destino.NumeroCuenta);
+            }
+        }
+
+
+    }
+}
