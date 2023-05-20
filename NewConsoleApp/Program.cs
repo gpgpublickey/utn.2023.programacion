5e9296828dc7a6959a3e1f13bbadd1ec522c09a2
delliLucas1 <dellilucas@gmail.com>
2023-05-19 22:03:59 -0300

    Ejercicio 19/5

--- NewConsoleApp/Program.cs
+++ NewConsoleApp/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var park1 = new Park();
+
+            park1.SetName("Plaza del Avion");
+            park1.SetAddress("Finca 6220");
+            park1.SetPeople(20);
+            park1.SetGames(3);
+
+            Console.WriteLine("Park Name: " + park1.GetName()); 
+            Console.WriteLine("Adrress: " + park1.GetAddress());
+            Console.WriteLine("Num of People: " + park1.GetPeople());
+            Console.WriteLine("Num of Games: " + park1.GetGames());
         }
     }
 }
