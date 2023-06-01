f6ee54354291d17e0e93b3f18b73791c0c188627
Mati279 <104004760+Mati279@users.noreply.github.com>
2023-06-01 09:52:13 -0300

    Programas individuales - Tarea de operadores
    Hice los programas separados porque así estaba en la consigna, si preferís que los junte en un solo programa decime y lo hago! Me queda pendiente hacer la calculadora. Saludos.

---
+++ Ejercicio Operadores/Programa farenheit/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Programa_farenheit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*No se maneja la posibilidad de que el usuario ingrese un input incorrecto, creo que eso requeriría
+                     * Try y Catch y algún loop, pero como no los vimos lo dejo como está*/
+
+
+            double celsius = 0;
+            double resultado;
+
+
+            Console.WriteLine("Escribe los grados celsius:");
+            celsius = Convert.ToDouble(Console.ReadLine());
+
+
+            resultado = celsius * 9 / 5 + 32;
+            Console.WriteLine("El resultado es " + resultado + ".");
+        }
+    }
+}
---
+++ Ejercicio Operadores/Programa triángulo/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*No se maneja la posibilidad de que el usuario ingrese un input incorrecto, creo que eso requeriría
+                 * Try y Catch y algún loop, pero como no los vimos lo dejo como está*/
+
+
+        double @base = 0;
+        double altura = 0;
+        double resultado;
+
+
+        Console.WriteLine("Escribe la base:");
+        @base = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Escribe la altura:");
+        altura = Convert.ToDouble(Console.ReadLine());
+
+        resultado = (@base * altura) / 2; 
+        Console.WriteLine("El resultado es " + resultado + ".");
+    }
+}
