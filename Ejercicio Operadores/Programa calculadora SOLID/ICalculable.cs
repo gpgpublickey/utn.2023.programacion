485fbc752b382298b44160fa7e906056ffe09ae2
Mati279 <104004760+Mati279@users.noreply.github.com>
2023-06-02 09:59:38 -0300

    Calculadora SOLID

---
+++ Ejercicio Operadores/Programa calculadora SOLID/ICalculable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa_calculadora_SOLID
+{
+    internal interface ICalculable
+    {
+
+        double Calcular(double n1, double n2);
+    }
+}
