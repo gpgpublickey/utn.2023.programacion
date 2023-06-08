2b5d57f8f903a5ba05d3704466d2846e1d5f9dbb
fedechiesa <fchiesah@gmail.com>
2023-06-08 16:04:23 -0300

    add operator project

---
+++ Calculator1/BaseCalculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator1
+{
+    internal class BaseCalculator : ICalculator
+    {
+        public abstract double Calculate(double a, double b);
+    }
+}
---
+++ Calculator1/ICalculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator1
+{
+    internal interface ICalculator
+    {
+        double Calcular(double a, double b);
+    }
+}
